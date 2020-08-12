40754cb3f4361f0d273c9bef72af8c949f59d6fe
pocharis <arinze6@gmail.com>
2020-08-12 19:31:13 +0100

    added calculations for ecd

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 from flask_pymongo import pymongo
 
 app = Flask(__name__)
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 client = pymongo.MongoClient("mongodb+srv://abdoul:abdoulonas@pochamb1-50wpb.mongodb.net/drillTechDB?retryWrites=true&w=majority")
 db = client.get_database('drillTechDB')
-
+collection = db.drillCalc
 
 #route to the home page
 @app.route('/')
@@ -27,11 +27,44 @@ def drilltech():
 #route to ecd page
 @app.route('/drilltech/ecd')
 def ecd():
-    collection = db.drillCalc
+    collection.delete_many({'calc_type':'ecd'})
+
     data = collection.find({'calc_type':'ecd'})
-    return render_template('index.html', data = data)
+    return render_template('ecd.html', data = data)
 
 
+#route to ecd calculations
+@app.route('/drilltech/ecd_calc', methods = ['POST', 'GET'])
+def ecd_calc():
+
+    ecd_drill = float(request.form['ecd_drill'])
+    mud_weight = int(request.form['mud_weight'])
+    depth = int(request.form['depth'])
+
+    surf_equip = int(request.form['surf_equip'])
+    in_drill_pipe = int(request.form['in_drill_pipe'])
+    in_drill_collars = int(request.form['in_drill_collars'])
+    bit_nozzles = int(request.form['bit_nozzles'])
+    annulus = int(request.form['annulus'])
+
+    total_pressure = surf_equip +  in_drill_collars + in_drill_pipe + bit_nozzles + annulus
+    ecd = round(mud_weight + (annulus / (0.052 * depth)), 2)
+
+    data = {'calc_type':'ecd',
+        'ecd_drill': ecd_drill,
+        'mud_weight': mud_weight,
+        'depth':depth,
+        'surf_equip':surf_equip,
+        'in_drill_pipe':in_drill_pipe,
+        'in_drill_collars':in_drill_collars,
+        'bit_nozzles':bit_nozzles,
+        'annulus':annulus,
+        'ecd':ecd,
+        'total_pressure':total_pressure
+    }
+
+    collection.insert(data)
+    return redirect(url_for('ecd', ecd = ecd, total_pressure = total_pressure ))
 
 
 
