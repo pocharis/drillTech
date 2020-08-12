a1be81c5e185012c4d9cd3ec896d6bd7fc4dca13
pocharis <arinze6@gmail.com>
2020-08-12 18:51:13 +0100

    added pymongo for mongoDB atlas

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
-from flask import Flask
+from flask import Flask, redirect, render_template
+from flask_pymongo import pymongo
+
 app = Flask(__name__)
 
+# CONNECTION_STRING = "mongodb+srv://test:test@flask-mongodb-atlas-1g8po.mongodb.net/test?retryWrites=true&w=majority"
+# client = pymongo.MongoClient(CONNECTION_STRING)
+# db = client.get_database('flask_mongodb_atlas')
+
+
+client = pymongo.MongoClient("mongodb+srv://abdoul:abdoulonas@pochamb1-50wpb.mongodb.net/drillTechDB?retryWrites=true&w=majority")
+db = client.get_database('drillTechDB')
+
+
+#route to the home page
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return render_template('index.html')
+
+
+#route to the drill tech page
+@app.route('/drilltech')
+def drilltech():
+    return render_template('drilltech.html')
+
+
+#route to ecd page
+@app.route('/drilltech/ecd')
+def ecd():
+    collection = db.drillCalc
+    data = collection.find({'calc_type':'ecd'})
+    return render_template('index.html', data = data)
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(port=5000,debug=True)
+
